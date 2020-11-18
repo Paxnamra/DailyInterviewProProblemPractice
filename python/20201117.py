d3d537e7ffb7e10df5bec749d9f157d21363d664
Nathan Li <nathanli1995@gmail.com>
2020-11-17 23:12:12 -0500

    added solution for 20201117

--- python/20201117.py
+++ python/20201117.py
@@ -1,13 +1,37 @@
+# Nov 17 2020
+# A k-ary tree is a tree with k-children, and a tree is symmetrical if the data of the left side of the tree is the same as the right side of the tree.
+
 class Node():
     def __init__(self, value, children=[]):
         self.value = value
         self.children = children
 
 def is_symmetric(root):
-    pass
+    left = extract_data_left(root.children[0])
+    right = extract_data_right(root.children[1])
+    if left == right:
+        return True
+    return False
+    # print(left)
+    # print(right)
 
-tree = Node(r)
+def extract_data_left(root):
+    if root.children == []:
+        return root.value
+    else:
+            return [root.value] + [ extract_data_left(i) for i in root.children ]
+
+def extract_data_right(root):
+    if root.children == []:
+        return root.value
+    else:
+            return [root.value] + [ extract_data_right(i) for i in reversed(root.children) ]
+
+
+
+tree = Node(4)
 tree.children = [Node(3), Node(3)]
 tree.children[0].children = [Node(9), Node(4), Node(1)]
 tree.children[1].children = [Node(1), Node(4), Node(9)]
-print( is_symmetric(tree))
+print(is_symmetric(tree))
+# is_symmetric(tree)
